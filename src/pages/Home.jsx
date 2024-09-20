49604af7b0f82944dcca2ebb5b23b7bc2dc1c014
Paloma Jellinek <palomacaviezel7@gmail.com>
2024-09-19 22:48:30 -0300

    popup file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ function Home() {
   return (
     <>
       <h2>Conoces a todos los Pokemones?</h2>
+      <p>Elegi los que mas te gusten y agregalos a tus favoritos!</p>
       <div className="pokemon-list">
         {pokemones.map((pokemon) => (
           <PokemonCard key={pokemon.name} {...pokemon} />
